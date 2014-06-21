134c6eb398d2179859dcc8d0bc1b8a129ccd0b90
Benjamin Shyong <ben.shyong@gmail.com>
2014-06-20 23:14:34 -0700

    Initial Commit

---
+++ Wisp/STAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  STAppDelegate.h
+//  Wisp
+//
+//  Created by Benjamin Shyong on 6/20/14.
+//  Copyright (c) 2014 ShyongTech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface STAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Wisp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Wisp
+//
+//  Created by Benjamin Shyong on 6/20/14.
+//  Copyright (c) 2014 ShyongTech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "STAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([STAppDelegate class]));
+    }
+}
